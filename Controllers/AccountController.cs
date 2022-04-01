5af54002ceadbf7539506e2801e47936791c3927
İmre Kosdik <55051877+imrekosdik@users.noreply.github.com>
2022-04-01 18:21:34 +0300

    Implemented some admin of the operations
    Delete user and see user details functionalities are added. 
    Problems related to login operations are fixed.
    Çağla Kaplan and İmre Kosdik have implemented the described operations together.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using LinkedHU_CENG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkedHU_CENG.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly Context _context = new Context();
+        public IActionResult RedirectToLogin()
+        {
+            return View("~/Views/Home/Login.cshtml");
+        }
+        public IActionResult RedirectToRegister()
+        {
+            return View("~/Views/Home/Register.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUser(Account account)
+        {
+            // implement popup for already registered users
+            var newUser = new Account(); // create new user instance
+            newUser.FirstName = account.FirstName;
+            newUser.LastName = account.LastName;
+            newUser.AccountType = account.AccountType;
+            newUser.Email = account.Email; // problem showing error messages for invalid email combinations
+            newUser.Password = account.Password;
+            newUser.IsAdmin = false;
+            if (ModelState.IsValid)
+            {
+                _context.Accounts.Add(newUser);
+                _context.SaveChanges();
+                return View("~/Views/homepage.cshtml");
+
+            }
+            return View("~/Views/Home/Register.cshtml");
+        }
+        
+        [HttpPost]
+        public IActionResult Login(Account account)
+        {
+            if (!String.IsNullOrEmpty(account.Email) && !String.IsNullOrEmpty(account.Password))
+            {
+                // check emails to see if the user is registered before
+                var user = _context.Accounts.Where(s => s.Email.Equals(account.Email)).ToList();
+                if (user.Any() && user[0].Password != account.Password) // if the user has entered a wrong password
+                {
+                    ViewBag.Text = "Password is Incorrect";
+                }
+                // if the user is not registered or the email entered is not correct
+                else if (!user.Any())
+                {
+                    ViewBag.Text = "You are not registered or you did not enter your email correctly.";
+                }
+                if (user.Any() && user[0].Password == account.Password)
+                {
+                    if (user[0].IsAdmin)
+                    {
+                        return new AdminController().ListUser(); 
+                    }
+                    return View("~/Views/homepage.cshtml"); // some user view - to be updated later
+                }
+            }
+            return View("~/Views/Home/Login.cshtml");
+            
+        }
+    }
+}
