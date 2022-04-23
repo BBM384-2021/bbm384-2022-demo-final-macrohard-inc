2b1a60b3a5c6ff4f4a80f128827c2b6f5457b859
Arda Cihaner <arda.cihaner@gmail.com>
2022-04-23 20:58:03 +0300

    Cleaning up following code

--- Controllers/FollowController.cs
+++ Controllers/FollowController.cs
@@ -1,6 +1,5 @@
 #nullable disable
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LinkedHUCENGv2.Data;
 using LinkedHUCENGv2.Models;
@@ -42,31 +41,6 @@ public class FollowController : Controller
         return View(follow);
     }
 
-    // GET: Follow/Create
-    public IActionResult Create()
-    {
-        ViewData["Account1Id"] = new SelectList(_context.Accounts, "Id", "Id");
-        ViewData["Account2Id"] = new SelectList(_context.Accounts, "Id", "Id");
-        return View();
-    }
-
-    // POST: Follow/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,DateCreated,Account1Id,Account2Id")] Follow follow)
-    {
-        if (ModelState.IsValid)
-        {
-            _context.Add(follow);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        ViewData["Account1Id"] = new SelectList(_context.Accounts, "Id", "Id", follow.Account1Id);
-        ViewData["Account2Id"] = new SelectList(_context.Accounts, "Id", "Id", follow.Account2Id);
-        return View(follow);
-    }
 
     [HttpGet]
     [ActionName("IsFollowed")]
@@ -160,36 +134,6 @@ public class FollowController : Controller
             return NotFound();
         _context.Remove(follow.Result);
         return RedirectToAction("Index");
-    }
-
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var follow = await _context.Follows
-            .Include(f => f.Account1)
-            .Include(f => f.Account2)
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (follow == null)
-        {
-            return NotFound();
-        }
-
-        return View(follow);
-    }
-
-    // POST: Follow/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        var follow = await _context.Follows.FindAsync(id);
-        _context.Follows.Remove(follow);
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
     }
 
     private bool FollowExists(int id)
