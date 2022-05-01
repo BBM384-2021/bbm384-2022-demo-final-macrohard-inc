83200393fa23019245b30bc95c6b48ac30e4405d
İmre Kosdik <55051877+imrekosdik@users.noreply.github.com>
2022-05-01 15:30:17 +0300

    Follow frontend connected to backend
    Viewing followers and followings are available.
    Viewing number of followers and followings are available.
    Fixed a bug in ProfileController.

---
+++ FollowController.cs
@@ -0,0 +1,155 @@
+#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LinkedHUCENGv2.Data;
+using LinkedHUCENGv2.Models;
+
+namespace LinkedHUCENGv2.Controllers;
+public class FollowController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public FollowController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: Follow
+    public async Task<IActionResult> Index()
+    {
+        var applicationDbContext = _context.Follows.Include(f => f.Account1).Include(f => f.Account2);
+        return View(await applicationDbContext.ToListAsync());
+    }
+
+    // GET: Follow/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var follow = await _context.Follows
+            .Include(f => f.Account1)
+            .Include(f => f.Account2)
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (follow == null)
+        {
+            return NotFound();
+        }
+
+        return View(follow);
+    }
+
+
+    [HttpGet]
+    [ActionName("IsFollowed")]
+    public bool IsUserFollowed(string userId, string userIdToFollow)
+    {
+        var followingUsers = _context.Follows.Where(a => a.Account1Id == userId);
+        foreach (var following in followingUsers)
+        {
+            if (userIdToFollow == following.Account2Id)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int GetFollowingCount(string userId)
+    {
+        var followingUsers = _context.Follows.Where(a => a.Account1Id == userId).ToList();
+        return followingUsers.Count;
+    }
+    
+
+    public int GetFollowerCount(string userId)
+    {
+        var followerUsers = _context.Follows.Where(a => a.Account2Id == userId).ToList();
+        return followerUsers.Count;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetFollowingList(string userId)
+    {
+        var followings = await _context.Follows.Where(a => a.Account1Id == userId).ToListAsync();
+        var followingUsers = new List<Account>();
+        foreach(var follow in followings)
+        {
+            var user = await _context.Accounts.Where(a => a.Id == follow.Account2Id).FirstOrDefaultAsync();
+            followingUsers.Add(user);
+        }
+        return View("~/Views/Follow/ListAccounts.cshtml", followingUsers);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetFollowersList(string userId)
+    {
+        var followers = await _context.Follows.Where(a => a.Account2Id == userId).ToListAsync();
+        var followerUsers = new List<Account>();
+        foreach(var follow in followers)
+        {
+            var user = await _context.Accounts.Where(a => a.Id == follow.Account1Id).FirstOrDefaultAsync();
+            followerUsers.Add(user);
+        }
+        
+        return View("~/Views/Follow/ListAccounts.cshtml", followerUsers);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> FollowUser(string userId)
+    {
+        var userToFollow = await _context.Accounts.Where(m => m.Id == userId)
+            .FirstOrDefaultAsync();
+        var currUser = await _context.Accounts.Where(m => m.Email == User.Identity.Name)
+            .FirstOrDefaultAsync();
+
+        if (userToFollow is null)
+            return NotFound();
+        if (currUser is null)
+            return NotFound();
+
+        if (IsUserFollowed(currUser.Id, userId))
+        {
+            return NotFound();
+        }
+        
+        var follow = new Follow
+        {
+            Account1 = currUser,
+            Account1Id = currUser.Id,
+            Account2 = userToFollow,
+            Account2Id = userToFollow.Id,
+            DateCreated = DateTime.Now
+        };
+        _context.Add(follow);
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UnfollowUser(string userId)
+    {
+        var userToUnfollow =  await _context.Accounts.Where(m => m.Id == userId)
+            .FirstOrDefaultAsync();
+        var currUser = await _context.Accounts.Where(m => m.Email == User.Identity.Name)
+            .FirstOrDefaultAsync();
+        
+        if (userToUnfollow is null)
+            return NotFound();
+        if (currUser is null)
+            return NotFound();
+
+        var follow = _context.Follows.Where(f => f.Account1.Id == currUser.Id && f.Account2.Id == userId).FirstOrDefaultAsync();
+        if (follow.Result is null)
+            return NotFound();
+        _context.Remove(follow.Result);
+        return RedirectToAction("Index");
+    }
+
+    private bool FollowExists(int id)
+    {
+        return _context.Follows.Any(e => e.Id == id);
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using LinkedHUCENGv2.Data;
+using Microsoft.AspNetCore.Mvc;
+using LinkedHUCENGv2.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkedHUCENGv2.Controllers;
+
+[Authorize]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private ApplicationDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger,
+                          ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Index()
+    {
+        if (!User.Identity.IsAuthenticated) return View();
+        var currAcc = await _context.Accounts.Where(m => m.Email == User.Identity.Name)
+            .FirstOrDefaultAsync();
+        return currAcc.IsAdmin ? RedirectToAction("Index", "Admin") : RedirectToAction("Homepage");
+    }
+
+
+
+    public async Task<IActionResult> Homepage()
+    {
+        var currAcc = await _context.Accounts.Where(m => m.Email == User.Identity.Name)
+            .FirstOrDefaultAsync();
+        if (currAcc is null)
+            return RedirectToAction("Login", "Account");
+        var followControl = new FollowController(_context);
+        var userProfileModel = new UserProfileModel
+        {
+            Id = currAcc.Id,
+            FirstName = currAcc.FirstName,
+            LastName = currAcc.LastName,
+            ProfileBio = currAcc.ProfileBio,
+            Phone = currAcc.Phone,
+            Url = currAcc.Url,
+            ProfilePhoto = currAcc.ProfilePhoto,
+            FollowersCount = followControl.GetFollowerCount(currAcc.Id),
+            FollowingCount = followControl.GetFollowingCount(currAcc.Id),
+            StudentNumber = currAcc.StudentNumber
+        };
+        return View(userProfileModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(string id, [Bind("FirstName,LastName,Phone,Url, ProfileBio")] Account account)
+    {
+        var user = await _context.Accounts.FindAsync(id);
+        if (user is null)
+            return NotFound();
+        if (id != user.Id)
+            return NotFound();
+
+        ModelState.Remove("AccountType");
+        if (ModelState.IsValid)
+        {
+            user.FirstName = account.FirstName;
+            user.LastName = account.LastName;
+            user.Phone = account.Phone;
+            user.Url = account.Url;
+            user.ProfileBio = account.ProfileBio;
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        return RedirectToAction("Index", "Home");
+    }
+
+    [AllowAnonymous]
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [AllowAnonymous]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
