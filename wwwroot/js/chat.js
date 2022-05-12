3f2854e0b26567388bfd36c5b60875427001712e
ilayda <iruyadachan@gmail.com>
2022-05-12 16:48:46 +0300

    Chat frontend fixed

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -7,10 +7,16 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
+    var encodedMsg = user + " : " + msg;
+    var div = document.createElement("div");
+    div.style.borderRadius = "30px";
+    div.style.backgroundColor = "white";
+    div.style.margin = "15px 0 15px 0";
+    div.style.padding = "8px";
+    var li = document.createElement("p");
     li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    div.appendChild(li);
+    document.getElementById("messagesList").appendChild(div);
 });
 
 connection.start().then(function () {
